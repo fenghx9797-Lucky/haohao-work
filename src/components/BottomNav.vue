915a39ad01eb8370bb8afd55c5cb968f9298eba9
fenghx9797-Lucky <fenghx9797@gmail.com>
2026-04-24 16:30:37 +0800

    Add project files

---
+++ src/components/BottomNav.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="bottom-nav">
+    <RouterLink
+      v-for="item in navItems"
+      :key="item.to"
+      :to="item.to"
+      class="bottom-nav__item"
+      active-class="bottom-nav__item--active"
+    >
+      <span class="bottom-nav__label">{{ item.label }}</span>
+      <span v-if="item.to === '/cart' && cartStore.totalCount" class="bottom-nav__badge">
+        {{ cartStore.totalCount }}
+      </span>
+    </RouterLink>
+  </nav>
+</template>
+
+<script setup>
+import { useCartStore } from '../stores/cart'
+
+const cartStore = useCartStore()
+
+const navItems = [
+  { label: '点单', to: '/' },
+  { label: '购物车', to: '/cart' },
+  { label: '订单', to: '/orders' }
+]
+</script>
+
+<style scoped>
+.bottom-nav__item {
+  position: relative;
+}
+
+.bottom-nav__badge {
+  position: absolute;
+  top: 6px;
+  right: 18px;
+  min-width: 18px;
+  height: 18px;
+  padding: 0 5px;
+  border-radius: 999px;
+  background: #ff4d4f;
+  color: #fff;
+  font-size: 11px;
+  line-height: 18px;
+}
+</style>
