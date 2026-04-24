a0ff3a51376344fb74560d86b2724476037fe88b
fenghx9797-Lucky <fenghx9797@gmail.com>
2026-04-24 16:30:27 +0800

    Add project files

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app-shell">
+    <div class="mobile-frame">
+      <main class="page-content">
+        <RouterView />
+      </main>
+      <BottomNav />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+
+import BottomNav from './components/BottomNav.vue'
+</script>
