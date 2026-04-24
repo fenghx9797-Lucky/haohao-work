76f5b0295f34b76f843bf86d9d02978fb6f24916
fenghx9797-Lucky <fenghx9797@gmail.com>
2026-04-24 16:30:33 +0800

    Add project files

---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import CartPage from '../pages/CartPage.vue'
+import CheckoutPage from '../pages/CheckoutPage.vue'
+import HomePage from '../pages/HomePage.vue'
+import OrdersPage from '../pages/OrdersPage.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomePage,
+    meta: { title: '点单' }
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: CartPage,
+    meta: { title: '购物车' }
+  },
+  {
+    path: '/checkout',
+    name: 'checkout',
+    component: CheckoutPage,
+    meta: { title: '下单成功' }
+  },
+  {
+    path: '/orders',
+    name: 'orders',
+    component: OrdersPage,
+    meta: { title: '我的订单' }
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+router.afterEach((to) => {
+  if (to.meta?.title) {
+    document.title = `${to.meta.title} | 咖啡奶茶点单`
+  }
+})
+
+export default router
