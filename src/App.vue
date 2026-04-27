35d0cae22ce6cbbcdade025542f9ffb2a8ca6971
fenghx9797-Lucky <fenghx9797@gmail.com>
2026-04-27 10:17:57 +0800

    Add splash-aware app shell

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="app-shell">
-    <div class="mobile-frame">
-      <main class="page-content">
+    <div class="mobile-frame" :class="{ 'mobile-frame--splash': isSplash }">
+      <main class="page-content" :class="{ 'page-content--splash': isSplash }">
         <RouterView />
       </main>
-      <BottomNav />
+      <BottomNav v-if="!isSplash" />
     </div>
   </div>
 </template>
 
 <script setup>
-import { RouterView } from 'vue-router'
+import { computed } from 'vue'
+import { RouterView, useRoute } from 'vue-router'
 
 import BottomNav from './components/BottomNav.vue'
+
+const route = useRoute()
+const isSplash = computed(() => route.name === 'splash')
 </script>
